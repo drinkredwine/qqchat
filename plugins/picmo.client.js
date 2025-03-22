934ab41490820233f887aea900ae028e1572df1f
Jozo Kovac <jozo.kovac@gmail.com>
2025-03-22 22:18:31 +0100

    Fix picmo CSS import paths

--- plugins/picmo.client.js
+++ plugins/picmo.client.js
@@ -1,5 +1,6 @@
 // Import the Picmo styles
-import 'picmo/themes/light.css';
+import 'picmo/dist/index.css';
+import '@picmo/popup-picker/dist/index.css';
 
 export default defineNuxtPlugin(() => {
   // Plugin is empty, just needed to import the CSS
